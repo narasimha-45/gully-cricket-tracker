77335d8ec7ea3cf1d859bd4f48c8f7d62f846125
narasimha <pappulanarasimha371@gmail.com>
2026-01-12 07:41:29 +0530

    vite.config.js updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import "dotenv/config"
+
+const API_BASE = process.env.API_BASE_URL;
+
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: API_BASE,
         changeOrigin: true
       }
     }
